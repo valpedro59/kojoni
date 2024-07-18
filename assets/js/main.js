554294249718a40ad64ea9ea407c9bababf9c8bf
valpedro59 <valpedro59@gmail.com>
2024-07-18 21:14:11 +0100

    90%

--- assets/js/main.js
+++ assets/js/main.js
@@ -199,6 +199,41 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
   });
+  /**
+   * Tech Slider
+   */
+  new Swiper('.tech-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 40
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 60
+      },
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 80
+      },
+      992: {
+        slidesPerView: 6,
+        spaceBetween: 120
+      }
+    }
+  });
 
   /**
    * Init swiper slider with 1 slide at once in desktop view
